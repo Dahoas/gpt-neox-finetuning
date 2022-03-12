521b6e0c41249b5af666afb1c611bd2a3e8df6ea
Ubuntu <tlieberum@outlook.de>
2022-03-12 22:48:51 +0000

    update

--- myscripts/tokenizer_downloads.sh
+++ myscripts/tokenizer_downloads.sh
@@ -1,3 +1,4 @@
+mkdir data
 wget https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-vocab.json
 wget https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-merges.txt
 mv gpt2-vocab.json data
