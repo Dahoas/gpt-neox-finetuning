f2571352e84336c709d972f5163ce56775ed92f7
Ubuntu <tlieberum@outlook.de>
2022-03-12 22:45:10 +0000

    tokenizer scripts

---
+++ myscripts/tokenizer_downloads.sh
@@ -0,0 +1,4 @@
+wget https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-vocab.json
+wget https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-merges.txt
+mv gpt2-vocab.json data
+mv gpt2-merges.txt data
